0c1734c605851a360b9d61b0853379d081008c9b
Jake Kara <jake@jakekara.com>
2019-12-13 10:50:53 -0500

    Updates

---
+++ precache-manifest.9591b81f6bb6823bc858afa7d90f4eb1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc159b890e479dd54086e0e71f5e9fc",
+    "url": "/citationdb/index.html"
+  },
+  {
+    "revision": "809b13bb26a737b41c7e",
+    "url": "/citationdb/static/css/2.5ec2c46e.chunk.css"
+  },
+  {
+    "revision": "b54154cf094d54aa1d13",
+    "url": "/citationdb/static/css/main.0b57dec3.chunk.css"
+  },
+  {
+    "revision": "809b13bb26a737b41c7e",
+    "url": "/citationdb/static/js/2.52478a68.chunk.js"
+  },
+  {
+    "revision": "b54154cf094d54aa1d13",
+    "url": "/citationdb/static/js/main.2ba078c6.chunk.js"
+  },
+  {
+    "revision": "3f9d1f7614451bd7a181",
+    "url": "/citationdb/static/js/runtime~main.cd3f41e9.js"
+  },
+  {
+    "revision": "ab2c7571e384b44a9239c04088a2f639",
+    "url": "/citationdb/static/media/dropdown-arrow.ab2c7571.svg"
+  },
+  {
+    "revision": "0d20b8076b76513b134aea5ba1ac597e",
+    "url": "/citationdb/static/media/search-icon.0d20b807.svg"
+  },
+  {
+    "revision": "05b91349df87902b2ab9534135ac7aa8",
+    "url": "/citationdb/static/media/x-circle-icon.05b91349.svg"
+  },
+  {
+    "revision": "630658872973a91efd5ae4b465f17e24",
+    "url": "/citationdb/static/media/x-field.63065887.svg"
+  },
+  {
+    "revision": "470d0275c316b4e7700106e2f06687de",
+    "url": "/citationdb/static/media/yalenew-roman-webfont.470d0275.woff"
+  },
+  {
+    "revision": "f2b260ad19889584280d4dfa48cde97f",
+    "url": "/citationdb/static/media/yalenew-roman-webfont.f2b260ad.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fortunoff-citationdb/precache-manifest.19b04466a055b93934694df69fbab657.js"
+  "/citationdb/precache-manifest.9591b81f6bb6823bc858afa7d90f4eb1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fortunoff-citationdb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/citationdb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
